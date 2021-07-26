b6cbb19a4d83c6a986b32cb892d43d8578432b99
Stefano <stefano.funaro@gmail.com>
2021-07-26 16:35:08 +0200

    campo_minato_funzionante_to-do_isNan-limitare_num

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
 var mines = [0];
+var userTries = [0]
 var flag = false;
 var rndNumber = 0;
 var m = 0; //m stands for mines index
+var u = 0; //u stands for userTries index
+var maxTries = 100 - 16;
 function rndGenerator() {
 return Math.floor(Math.random() * 100) + 1;
 }
@@ -18,7 +21,28 @@ do {
 while (m < 16)
 console.log(mines);
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+do {
+    var userNum = parseInt(prompt("per favore inserisca un numero da 1 a 100"));
+    if(!mines.includes(userNum) && !userTries.includes(userNum) && u < maxTries) {
+    userTries[u] = userNum ;
+    u++
+    }
+    else if (userTries.includes(userNum)){
+        var userNum = parseInt(prompt("per favore inserisca un numero da 1 a 100, il precedente numero era già stato inserito"));
+    }
+    else if (u == maxTries ) {
+        alert("hai visto hai totalizzato il punteggio Massimo: " + userTries.length);
+        flag = true
+    }
+    else{
+    flag = true
+    alert("mi dispiace hai perso! Hai effettuato un punteggio di: " + (userTries.length));
+    }
+}
+while (u <= maxTries && flag == false)
+console.log(userTries);
 // L’utente non può inserire più volte lo stesso numero.
+
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
