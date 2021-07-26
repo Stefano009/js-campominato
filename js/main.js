5b786ef1f669373392cd412f9e59253eeb213d56
Stefano <stefano.funaro@gmail.com>
2021-07-26 17:21:18 +0200

    funzionante_con_per_num_da_1a100_e_non_accetta_NaN

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var rndNumber = 0;
 var m = 0; //m stands for mines index
 var u = 0; //u stands for userTries index
 var maxTries = 100 - 16;
+var userNum = parseInt(prompt("per favore inserisca un numero da 1 a 100"));
 function rndGenerator() {
 return Math.floor(Math.random() * 100) + 1;
 }
@@ -22,22 +23,49 @@ while (m < 16)
 console.log(mines);
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 do {
-    var userNum = parseInt(prompt("per favore inserisca un numero da 1 a 100"));
-    if(!mines.includes(userNum) && !userTries.includes(userNum) && u < maxTries) {
-    userTries[u] = userNum ;
-    u++
-    }
-    else if (userTries.includes(userNum)){
-        var userNum = parseInt(prompt("per favore inserisca un numero da 1 a 100, il precedente numero era già stato inserito"));
-    }
-    else if (u == maxTries ) {
-        alert("hai visto hai totalizzato il punteggio Massimo: " + userTries.length);
-        flag = true
-    }
-    else{
-    flag = true
-    alert("mi dispiace hai perso! Hai effettuato un punteggio di: " + (userTries.length));
-    }
+     
+    // if(!mines.includes(userNum) && !userTries.includes(userNum) && u < maxTries) {
+    // userTries[u] = userNum ;
+    // u++
+    // }
+    // else if (userTries.includes(userNum)){
+    //     var userNum = parseInt(prompt("per favore inserisca un numero da 1 a 100, il precedente numero era già stato inserito"));
+    // }
+    // else if (u == maxTries ) {
+    //     alert("hai visto hai totalizzato il punteggio Massimo: " + userTries.length);
+    //     flag = true
+    // }
+    // else{
+    // flag = true
+    // alert("mi dispiace hai perso! Hai effettuato un punteggio di: " + (userTries.length));
+    // } 
+    switch(u <= maxTries){        
+        case !mines.includes(userNum) && !userTries.includes(userNum) && u < maxTries && !isNaN(userNum):
+            userTries[u] = userNum ;
+            u++;
+            break;
+        case userTries.includes(userNum):            
+            alert("per favore inserisca un numero da 1 a 100, il precedente numero era già stato inserito");
+            break;
+        case isNaN(userNum) == true:
+            alert("per favore inserisca un numero da 1 a 100, il precedente numero era NaN");
+            break;
+        case userNum > 100:
+            alert("per favore inserisca un numero da 1 a 100, il precedente numero non era compreso in questo range");
+            break;
+        case userNum < 1:
+            alert("per favore inserisca un numero da 1 a 100, il precedente numero non era compreso in questo range");
+            break;
+        case u == maxTries:
+            alert("hai visto hai totalizzato il punteggio Massimo: " + userTries.length);
+            break;
+        default:
+            flag = true
+            alert("mi dispiace hai perso! Hai effettuato un punteggio di: " + (userTries.length));
+        }
+        if (u <= maxTries && flag == false)
+        userNum = parseInt(prompt("per favore inserisca un numero da 1 a 100"));
+        
 }
 while (u <= maxTries && flag == false)
 console.log(userTries);
